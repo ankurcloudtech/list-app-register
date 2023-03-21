a469406baf036c718c2059e4e84af16aa2a5c91f
ankur51206 <32640688+ankur51206@users.noreply.github.com>
2023-03-22 00:16:30 +0530

    Update list.ps1

--- list.ps1
+++ list.ps1
@@ -1,53 +1,42 @@
-# Connect to Azure
-Connect-AzAccount
+Connect-AzureAD
+$Applications = Get-AzureADApplication -all $true
+$Logs = @()
+Write-host "Applications with the Secrets and Certificates that expire in the next 30 days" -ForegroundColor Green
 
-# Get a list of subscriptions
-$subList = Get-AzSubscription
+foreach ($app in $Applications) {
+    $AppName = $app.DisplayName
+    $AppID = $app.objectid
+    $ApplID = $app.AppId
+    $AppCreds = Get-AzureADApplication -ObjectId $AppID | select PasswordCredentials
+    $secret = $AppCreds.PasswordCredentials
 
-# Loop through each subscription
-foreach ($subscription in $subList) {
-    Write-Host "Subscription Name: $($subscription.Name)"
-    Write-Host "Subscription ID: $($subscription.Id)"
+    foreach ($s in $secret) {
+        $StartDate = $s.StartDate
+        $EndDate = $s.EndDate
+        $operation = $EndDate - (Get-Date)
+        $ODays = $operation.Days
 
-    # Select the current subscription context
-    Set-AzContext -Subscription $subscription.Id
-
-    # Get a list of app registrations in the current subscription
-    $appList = Get-AzADApplication
-
-    # Loop through each app registration
-    foreach ($appRegistration in $appList) {
-        Write-Host "App Name: $($appRegistration.DisplayName)"
-        Write-Host "App ID: $($appRegistration.ApplicationId)"
-
-        # Check if the ObjectId or ApplicationId is empty
-        if (!$appRegistration.Id -or !$appRegistration.ApplicationId) {
-            Write-Host "ObjectId or ApplicationId is empty, skipping app registration"
-            Write-Host ""
-            continue
-        }
-
-        # Check if there are any expiring ClientSecrets
-        $expiringSecrets = $appRegistration.PasswordCredentials | Where-Object {$_.EndDate -gt (Get-Date).AddDays(30)}
-
-        # Check if there are any expiring Certificates
-        $expiringCertificates = $appRegistration.KeyCredentials | Where-Object {$_.EndDate -gt (Get-Date).AddDays(30)}
-
-        # Output the credential information if there are any expiring secrets or certificates
-        if ($expiringSecrets.Count -gt 0 -or $expiringCertificates.Count -gt 0) {
-            Write-Host "Expiring ClientSecrets:"
-            foreach ($secret in $expiringSecrets) {
-                $expiry_diff = ($secret.EndDate - (Get-Date)).Days
-                Write-Host "- Secret: $($secret.KeyId), Expires: $($secret.EndDate), Days Remaining: $expiry_diff"
+        if ($ODays -le 30 -and $ODays -ge 0) {
+            $Log = [PSCustomObject]@{
+                ApplicationName      = $AppName
+                ApplicationID        = $ApplID
+                SecretStartDate      = $StartDate
+                SecretEndDate        = $EndDate
+                CertificateStartDate = ""
+                CertificateEndDate   = ""
+                Owner                = ""
             }
-            Write-Host ""
 
-            Write-Host "Expiring Certificates:"
-            foreach ($certificate in $expiringCertificates) {
-                $expiry_diff = ($certificate.EndDate - (Get-Date)).Days
-                Write-Host "- Certificate: $($certificate.KeyId), Expires: $($certificate.EndDate), Days Remaining: $expiry_diff"
+            $Owner = Get-AzureADApplicationOwner -ObjectId $app.ObjectId
+            if ($Owner.UserPrincipalName -eq $Null) {
+                $Log.Owner = $Owner.DisplayName + " **<This is an Application>**"
+            } else {
+                $Log.Owner = $Owner.UserPrincipalName
             }
-            Write-Host ""
+
+            $Logs += $Log
         }
     }
 }
+
+$Logs | Format-Table -AutoSize

8504804b1904e73f3702c87215af895e592c0577
ankur51206 <32640688+ankur51206@users.noreply.github.com>
2023-03-27 22:33:23 +0530

    Update list.ps1

--- list.ps1
+++ list.ps1
@@ -1,42 +1,46 @@
 param(
     $daysLogon = 30
 )
+
 Import-Module AzureAD
 
 function email_error() {
     param(
         $err1
     )
+    
     # Default email variables
     $EmailFrom = ""
     $EmailTo = @("")
     $EmailSubject = ""
     $SMTPserver = ""
     $script_time = Get-Date
-    $emailBody = "<html><body><br>
-    <font color='FF0000'>Run at $script_time</font><br /><br />
-    <table cellpadding='5' cellspacing='0' style='border: 1px solid black;'>
-    <tr><th style='border: 1px solid black;'>App Name</th><th style='border: 1px solid black;'>Object Id</th><th style='border: 1px solid black;'>Type</th><th style='border: 1px solid black;'>Certificate Name</th><th style='border: 1px solid black;'>Days to Expiry</th></tr>
-    $err1
-    </table>"
+
+    # Create HTML table
+    $table = "<table><tr><th>App Name</th><th>Object Id</th><th>Credential Type</th><th>Credential Name</th><th>Expiring in (Days)</th></tr>"
+    $table += $err1
+    $table += "</table>"
+
     # Default sendmail parameters
     $sendMailParameters = @{
         From       = $EmailFrom
         To         = $EmailTo
         Subject    = $EmailSubject
-        Body       = $emailBody
+        Body       = $table
         SMTPServer = $SMTPserver
         BodyAsHTML = $True
     }
+    
     # Send the email
     Send-MailMessage @sendMailParameters
 }
 
+# Initialize output variable
+$message = ""
+
 # Expiring App Registration Certs and Certificates
 $Applications = Get-AzureADApplication -all $true
 $now = get-date
-
-$tableRows = @()
 
 foreach ($app in $Applications) {
     $AppName = $app.DisplayName
@@ -50,17 +54,30 @@ foreach ($app in $Applications) {
         $StartDate = $s.StartDate
         $EndDate = $s.EndDate
         $Logs = $EndDate - $now
-        if (($Logs.Days -lt $daysLogon) -and ($Logs.Days -gt 0))
-        {
-            $tableRows += "<tr><td style='border: 1px solid black;'>$AppName</td><td style='border: 1px solid black;'>$AppID</td><td style='border: 1px solid black;'>Secret</td><td style='border: 1px solid black;'></td><td style='border: 1px solid black; color:yellow'>$($Logs.Days) Days</td></tr>"
+        
+        if (($Logs.Days -lt $daysLogon) -and ($Logs.Days -gt 0)) {
+            # Append output to message
+            $message += "<tr><td>$AppName</td><td>$AppID</td><td>Secret</td><td>$($s.DisplayName)</td><td>$($Logs.Days)</td></tr>"
         }
     }
+
     foreach ($c in $cert) {
         $StartDate = $c.StartDate
         $EndDate = $c.EndDate
         $Logs = $EndDate - $now
-        if (($Logs.Days -lt $daysLogon) -and ($Logs.Days -gt 0))
-        {
-            $tableRows += "<tr><td style='border: 1px solid black;'>$AppName</td><td style='border: 1px solid black;'>$AppID</td><td style='border: 1px solid black;'>Certificate</td><td style='border: 1px solid black;'>$($c.DisplayName)</td><td style='border: 1px solid black; color:red'>$($Logs.Days) Days</td></tr>"
+        
+        if (($Logs.Days -lt $daysLogon) -and ($Logs.Days -gt 0)) {
+            # Append output to message
+            $message += "<tr><td>$AppName</td><td>$AppID</td><td>Certificate</td><td>$($c.DisplayName)</td><td>$($Logs.Days)</td></tr>"
         }
-   
+    }
+}
+
+# Check if there are any results to send
+if ($message -ne "") {
+    # Call email_error function to send email
+    email_error -err1 "$message"
+}
+else {
+    Write-Host "No expiring App Registration Certs and Certificates found."
+}
